555cb79bd398f22c7a75a39b53f121008b7257d4
Sammye Jones <sajones@ucdavis.edu>
2019-11-19 16:41:03 -0800

    added vars

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,31 @@
+var startBtn = document.querySelector("getreadybtn");
+var startBlock = document.querySelector("getready");
+var questionBlock = document.querySelector("card1");
+
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     {
         title: "_____ is the file type commonly used to modify style elements.",
         choices: ["Javascript", "HTML", "JQuery", "CSS"],
-        answer: "CSS"   
+        answer: "CSS"
     },
     {
         title: "Javascript variables that can store more than one piece of data are called _____.",
-      choices: ["Arrays", "Variables", "For loops", "Timeouts"],
-      answer: "Arrays"
+        choices: ["Arrays", "Variables", "For loops", "Timeouts"],
+        answer: "Arrays"
     },
     {
         title: "To properly render javascript, one should place their reference link in the end of HTML ____.",
         choices: ["body", "head", "header", "script"],
         answer: "body"
     }
-  ];
+];
